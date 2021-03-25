1e1e65680270e83dc83311aadf9a16f7c050547d
YousefAlhamed <yousefalhamed7@gmail.com>
2021-03-25 13:09:43 +0300

    Adding server component to project

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ mongoose
     console.log(err);
   });
 
-app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
+// Development
+// app.listen(3000, () => {
+//   console.log("server is running http://localhost:3000");
+// });
+
+// Production
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
